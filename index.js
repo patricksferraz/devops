e2687e25ca7e9ccd4062c65cefa04d441638e8b1
Patrick Ferraz <patrick.ferraz@outlook.com>
2019-04-24 00:02:05 -0300

    refactor(index): adds staging name in root route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send({ hello: "world, here is production" });
+    res.send({ hello: "world, here is staging" });
 });
 
 const PORT = process.env.PORT || 8080;
